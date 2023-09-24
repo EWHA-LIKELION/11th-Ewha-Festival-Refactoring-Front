a1cd9fd6a90470a2b2f6afc3a2c4427faccedd27
binys21 <binys1121@ewhain.net>
2023-09-25 00:45:05 +0900

    style: 네비게이션 css

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { styled } from "styled-components";
 //component
 import Booth from "../components/Booth";
-import Navigation from "../components/Navigation";
 //image
 import { ReactComponent as Namecover } from "../images/nickname.svg";
+import redhover from "../images/redhover.svg";
+import yellowhover from "../images/yellowhover.svg";
+import greenline from "../images/greenline.png";
 
 const MyPage = () => {
+  const [isBooth, setIsBooth] = useState(true);
+  const [likeBooth, setLikeBooth] = useState(true);
+  const [isCategory, setIsCategory] = useState(false);
+
+  const clickBooth = () => {
+    setIsBooth(true);
+  };
+  const clickConcert = () => {
+    setIsBooth(false);
+  };
+  const clickLikeBooth = () => {
+    setLikeBooth(true);
+  };
+  const clickLikeConcert = () => {
+    setLikeBooth(false);
+  };
+  const selectedCategory = () => {
+    selectedCategory(true);
+  };
+
   return (
     <Wrapper>
       <Namecover width={260} />
@@ -16,13 +38,33 @@ const MyPage = () => {
         <button>로그아웃</button>
       </NameCard>
 
-      <Navigation />
+      <Navigation>
+        <Top>
+          <div className="booth" onClick={clickBooth}>
+            부스
+          </div>
+          <div className="concert" onClick={clickConcert}>
+            공연
+          </div>
+        </Top>
+        <hr></hr>
+        <Bottom>
+          <div className="booth" onClick={clickLikeBooth}>
+            좋아요한 부스
+          </div>
+          <div className="menu" onClick={clickLikeConcert}>
+            좋아요한 메뉴
+          </div>
+        </Bottom>
+      </Navigation>
+
       <Category>
-        <span>전체 · </span>
-        <span>날짜 · </span>
-        <span>장소 · </span>
-        <span>카테고리</span>
+        <span onClick={selectedCategory}>전체 · </span>
+        <span onClick={selectedCategory}>날짜 · </span>
+        <span onClick={selectedCategory}>장소 · </span>
+        <span onClick={selectedCategory}>카테고리</span>
       </Category>
+
       <div className="count">총 100개의 부스</div>
       <List>
         <Booth />
@@ -83,10 +125,74 @@ const List = styled.div`
   margin-bottom: 64px;
 `;
 const Category = styled.div`
-  color: var(--gray2);
+  color: ${(props) => (props.isCategory ? "#F55B1D" : "#9B9B9B")};
   text-align: center;
   font-size: 15px;
   font-style: normal;
   font-weight: 400;
   margin-right: 179px;
+`;
+
+// 네비게이션
+const Navigation = styled.div`
+  width: 390px;
+  height: 85.688px;
+  hr {
+    width: 390px;
+    height: 1.04px;
+    border: 0;
+    background: #efefef;
+  }
+`;
+const Top = styled.div`
+  display: flex;
+  justify-content: center;
+  color: var(--gray2);
+  font-size: 16.64px;
+  line-height: 2.5;
+  font-weight: 500;
+  .booth {
+    text-align: center;
+    width: 60px;
+    background-image: ${(props) =>
+      props.isBooth ? `url(${yellowhover})` : "none"};
+    background-size: cover; // 배경 이미지 크기 조정
+    background-repeat: no-repeat;
+    color: ${(props) => (props.isBooth ? "#ffffff" : "#9b9b9b")};
+
+    margin-right: 25px;
+  }
+  .concert {
+    text-align: center;
+    width: 60px;
+    margin-left: 25px;
+    background-image: ${(props) =>
+      !props.isBooth ? `url(${yellowhover})` : "none"};
+    background-size: 59px 35.7px;
+    background-repeat: no-repeat; // 배경 이미지 반복 제거
+    color: ${(props) => (!props.isBooth ? "#029C54" : "#9B9B9B")};
+    font-weight: ${(props) => (!props.isBooth ? "700" : "500")};
+  }
+`;
+const Bottom = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+  color: var(--gray2);
+  line-height: 2.5;
+  font-size: 13px;
+  font-weight: 400;
+  .booth {
+    text-align: center;
+    width: 86.295px;
+    height: 31px;
+    margin-right: 12px;
+    background-image: ${(props) =>
+      !props.isBooth ? `url(${redhover})` : "none"};
+    color: ${(props) => (!props.isBooth ? "#ffff" : "#9B9B9B")};
+    font-weight: ${(props) => (!props.isBooth ? "600" : "500")};
+  }
+  .menu {
+    margin-left: 12px;
+  }
 `;
