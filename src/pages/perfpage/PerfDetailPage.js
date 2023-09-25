0d39516a797391ceedce5f7fd22c2b8e454a1fba
youtheyeon <yuseoyeon@ewhain.net>
2023-09-25 21:00:07 +0900

    feat : 공연 상세페이지 추가

---
+++ src/pages/perfpage/PerfDetailPage.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+//components
+import DetailCover from '../../components/Detail/DetailCover';
+import DetailTitle from '../../components/Detail/DetailTitle';
+import DetailInfo from '../../components/Detail/DetailInfo';
+import DetailReview from '../../components/Detail/DetailReview';
+
+const BoothDetailPage = () => {
+  return (
+    <Wrapper>
+      <DetailCover />
+      <Content>
+        <DetailTitle event='공연' />
+        <DetailInfo event='공연' />
+        <DetailReview />
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default BoothDetailPage;
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background-color: #fff9f1;
+`;
+
+const Content = styled.div`
+  padding: 0px 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+`;
