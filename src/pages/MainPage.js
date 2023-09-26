63a35dba74098f6a5d543a6b0d87ee4830a44fef
kyubinjin <2376282@ewhain.net>
2023-09-26 15:33:56 +0900

    refactor: 이미지 리네이밍

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,34 +1,36 @@
 import React from "react";
 import styled from "styled-components";
 
-import MenuIcon from "../assets/MenuIcon.svg";
-import MainBanner from "../assets/MainBanner.svg";
-import BannerGreen from "../assets/BannerGreen.svg";
-import BannerBlue from "../assets/BannerGreen.svg";
-import TitleIcon from "../assets/TitleIcon.svg";
-import Schedule_1 from "../assets/Schedule_1.svg";
-import Schedule_2 from "../assets/Schedule_2.svg";
-import Schedule_3 from "../assets/Schedule_3.svg";
-import HighlightYellow from "../assets/HighlightYellow.svg";
-import FestivalLogo from "../assets/FestivalLogo.svg";
-import LikelionLogo from "../assets/LikelionLogo.svg";
+import menu from "../assets/icons/menu.svg";
+import mypageGreen from "../assets/images/Main/mypage-green.svg";
+import bannerMain from "../assets/images/Main/banner-main.svg";
+import bannerGreen from "../assets/images/Main/banner-green.svg";
+import bannerBlue from "../assets/images/Main/banner-blue.svg";
+import titleBackground from "../assets/icons/title-background.svg";
+import schedule_1 from "../assets/images/Main/schedule-1.svg";
+import schedule_2 from "../assets/images/Main/schedule-2.svg";
+import schedule_3 from "../assets/images/Main/schedule-3.svg";
+import highlightYellow from "../assets/icons/highlight-yellow.svg";
+import logoFestival from "../assets/images/Main/logo-festival.svg";
+import logoLikelion from "../assets/images/Main/logo-likelion.svg";
 
 const MainPage = () => {
   return (
     <Wrapper>
       <TopBar>
-        <img src={MenuIcon} />
+        <img src={menu} />
+        <img src={mypageGreen} />
       </TopBar>
-      <img src={MainBanner} />
-      <img src={BannerGreen} />
-      <img src={BannerBlue} />
+      <img src={bannerMain} />
+      <img src={bannerGreen} />
+      <img src={bannerBlue} />
       <EventPlan>
         <div>
-          <img src={TitleIcon} />
+          <img src={titleBackground} />
           <p>행사 일정</p>
         </div>
         <div>
-          <img src={Schedule_1} />
+          <img src={schedule_1} />
           <p>05.10</p>
           <p>이화인 한솥밥 배부</p>
           <p>12:00</p>
@@ -36,7 +38,7 @@ const MainPage = () => {
           <p>15:30</p>
         </div>
         <div>
-          <img src={Schedule_2} />
+          <img src={schedule_2} />
           <p>05.11</p>
           <p>이화의 볼륨을 높여</p>
           <p>17:30 - 18:00</p>
@@ -44,7 +46,7 @@ const MainPage = () => {
           <p>19:30</p>
         </div>
         <div>
-          <img src={Schedule_3} />
+          <img src={schedule_3} />
           <p>05.12</p>
           <p>이화의 목소리가 보여</p>
           <p>17:30 - 18:30</p>
@@ -54,7 +56,7 @@ const MainPage = () => {
       </EventPlan>
       <PerformPlan>
         <div>
-          <img src={TitleIcon} />
+          <img src={titleBackground} />
           <p>공연 일정표</p>
         </div>
       </PerformPlan>
