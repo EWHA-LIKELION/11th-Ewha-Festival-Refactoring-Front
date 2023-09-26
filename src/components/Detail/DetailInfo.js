5c4a3c5e01d4eaf99dee349d37ca2040d6fdb25b
youtheyeon <yuseoyeon@ewhain.net>
2023-09-26 21:51:34 +0900

    feat : 상세페이지 정보 더보기 토글 구현

--- src/components/Detail/DetailInfo.js
+++ src/components/Detail/DetailInfo.js
@@ -11,25 +11,38 @@ import arrow from '../../assets/icons/arrow-green3.svg';
 const DetailInfo = ({event}) => {
   const [thisPlace, setThisPlace] = useState('포스코관'); //임시
 
+  //정보 더보기(지도, 소개 전체보기) 토글 관리
+  const [isToggleOpen, setIsToggleOpen] = useState(false);
+
+  const toggleInfo = () => {
+    setIsToggleOpen(!isToggleOpen);
+  };
+
   return (
     <Section>
       <Header>
         <SecTitle sectitle={`${event} 정보`} />
-        <PopUpBtn src={arrow} />
+        <ToggleBtn src={arrow} onClick={toggleInfo} />
       </Header>
       <Line />
       <Content>
         <SecSub subtitle={`${event} 위치`} color='var(--green1)'>
           포스코관 01
         </SecSub>
-        <MapContainer>
-          <Map page='detail' place={thisPlace} />
-        </MapContainer>
+        {isToggleOpen && (
+          <MapContainer>
+            <Map page='detail' place={thisPlace} />
+          </MapContainer>
+        )}
         <SecSub subtitle={`${event} 운영시간`} color='var(--green1)'>
           <div>10일 수요일 - AM 10:00 ~ PM 5:00</div>
           <div>11일 목요일 - AM 10:00 ~ PM 5:00</div>
         </SecSub>
-        <SecSub subtitle={`${event} 소개`} color='var(--black)' overflow='on'>
+        <SecSub
+          subtitle={`${event} 소개`}
+          color='var(--black)'
+          overflow={isToggleOpen ? 'off' : 'on'}
+        >
           부스소개글부스소개글부스소개글부스소개글부스소개글부스소개글부스소개글부스소개글부스소개글부스소개글부스소개글부스소개글부스소개글부스소개글부스소개글부스소개글부스소개글부스소개글부스소개글
         </SecSub>
         <SecSub subtitle={`${event} 운영진 연락처`}>
@@ -56,11 +69,12 @@ const Header = styled.div`
   justify-content: space-between;
 `;
 
-const PopUpBtn = styled.img`
+const ToggleBtn = styled.img`
   margin-top: 27.6px;
   margin-right: 16.6px;
   width: 12.48px;
   height: 7.28px;
+  cursor: pointer;
 `;
 
 const Content = styled.div`
