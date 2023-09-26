b844eac9ddf2e543fb95758475f33f5329d2412a
youtheyeon <yuseoyeon@ewhain.net>
2023-09-26 17:40:28 +0900

    refactor : 상단바 타이틀 코드 변경

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import "./App.css";
+import './App.css';
 
-import MainPage from "./pages/MainPage";
-import BoothDetailPage from "./pages/BoothDetailPage";
-import PerfDetailPage from "./pages/PerfDetailPage";
-import BoothListPage from "./pages/BoothListPage";
+import MainPage from './pages/MainPage';
+import BoothDetailPage from './pages/BoothDetailPage';
+import PerfDetailPage from './pages/PerfDetailPage';
+import BoothListPage from './pages/BoothListPage';
 
 function App() {
   return (
     <>
-      <MainPage />
+      {/* <MainPage /> */}
+      {/* <BoothListPage /> */}
       <BoothDetailPage />
       <PerfDetailPage />
     </>
