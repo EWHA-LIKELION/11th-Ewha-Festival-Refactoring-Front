a322671adbde56d925572edf9b6dba55709eaba2
JiwonChoi0805 <won0805@ewhain.net>
2023-09-26 21:22:58 +0900

    [Feature] 수정페이지 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import './App.css';
+// import "./App.css";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import BoothEditPage from "./pages/BoothEditPage";
 
-function App() {
-  return <></>;
-}
+// function App() {
+//   return (
+//     <>
+//       <Router>
+//         <Routes>
+//           <Route path="/bootheditpage" element={<BoothEditPage />} />
+//         </Routes>
+//       </Router>
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
---
+++ src/pages/PerfEditPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const PerfEditPage = () => {
+    return (
+        <>
+            <Wrapper>
+                <div>공연 수정페이지</div>
+            </Wrapper>
+        </>
+    );
+};
+export default PerfEditPage;
+
+const Wrapper = styled.div`
+    background-color: var(--beige);
+    width: 100%;
+    height: 100%;
+`;
