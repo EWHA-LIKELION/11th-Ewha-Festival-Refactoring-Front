f9a889dcdff4b02cf24207a2887469720a92cc8a
youtheyeon <yuseoyeon@ewhain.net>
2023-10-02 23:03:26 +0900

    style : 배경 색 수정

--- src/pages/BoothDetailPage.js
+++ src/pages/BoothDetailPage.js
@@ -154,7 +154,7 @@ const Wrapper = styled.div`
   min-height: 100vh;
   display: flex;
   flex-direction: column;
-  background-color: #fff9f1;
+  background: var(--beige);
 `;
 
 const Content = styled.div`
