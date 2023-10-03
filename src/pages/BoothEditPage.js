4ccb4e1f18b56894c68d4af34a509bbdcc33a241
JiwonChoi0805 <won0805@ewhain.net>
2023-10-03 13:54:59 +0900

    feat: 수정페이지 연결 중

--- src/pages/BoothEditPage.js
+++ src/pages/BoothEditPage.js
@@ -9,8 +9,8 @@ const BoothEditPage = () => {
   return (
     <>
       <Wrapper>
-        {/* <EditBooth /> */}
-        {/* <EditMenu /> */}
+        {/*<EditBooth />*/}
+        {/*<EditMenu />*/}
         <EditMenuDetail />
       </Wrapper>
     </>
