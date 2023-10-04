9d94923c6cf062e840a8086b94fdc1487e63bbdd
kyubinjin <2376282@ewhain.net>
2023-10-05 01:29:57 +0900

    style: notice 줄바꿈 수정

--- src/components/MainPage/PerfPlanTable.js
+++ src/components/MainPage/PerfPlanTable.js
@@ -1,6 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 
-const PerfPlanTable = () => {};
+const PerfPlanTable = () => {
+  const columnData = [
+    {
+      time: "12:00~12:30",
+      club: "냠냠",
+    },
+  ];
+};
 
 export default PerfPlanTable;
