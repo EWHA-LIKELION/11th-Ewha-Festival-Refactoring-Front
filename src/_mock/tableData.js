22d702d2c8c842605a8a8188b5bcb0f6657ed11e
kyubinjin <2376282@ewhain.net>
2023-10-07 00:18:33 +0900

    feat: 공연일정표 완성

--- src/_mock/tableData.js
+++ src/_mock/tableData.js
@@ -128,3 +128,101 @@ export const tableData2 = [
     name: "이화의 볼륨을 높여",
   },
 ];
+
+export const tableData3 = [
+  {
+    id: 1,
+    time: "12:15~12:30",
+    name: "아워유스",
+  },
+  {
+    id: 2,
+    time: "13:45~14:00",
+    name: "라온소울",
+  },
+  {
+    id: 3,
+    time: "15:15~15:30",
+    name: "언타이틀",
+  },
+  {
+    id: 4,
+    time: "16:45~17:00",
+    name: "이화아카데미앙상블",
+  },
+  {
+    id: 5,
+    time: "18:15~18:30",
+    name: "Go Straight N' Turn Right",
+  },
+];
+
+export const tableData4 = [
+  {
+    id: 1,
+    time: "12:15~12:30",
+    name: "하이라이트",
+  },
+  {
+    id: 2,
+    time: "13:45~14:00",
+    name: "유니",
+  },
+  {
+    id: 3,
+    time: "15:15~15:30",
+    name: "프로젝트 403",
+  },
+  {
+    id: 4,
+    time: "16:45~17:00",
+    name: "밴드 호야",
+  },
+  {
+    id: 5,
+    time: "18:15~18:30",
+    name: "iMiM",
+  },
+];
+
+export const tableData5 = [
+  {
+    id: 1,
+    time: "12:15~12:30",
+    name: "이크에크",
+  },
+  {
+    id: 2,
+    time: "13:45~14:00",
+    name: "early summer club",
+  },
+  {
+    id: 3,
+    time: "15:15~15:30",
+    name: "착석가능",
+  },
+];
+
+export const tableData6 = [
+  {
+    id: 1,
+    time: "17:00~17:30",
+    name: "투일투 밴드",
+  },
+];
+
+export const tableData7 = [
+  {
+    id: 1,
+    time: "17:00~17:30",
+    name: "이화한결",
+  },
+];
+
+export const tableData8 = [
+  {
+    id: 1,
+    time: "18:30~19:00",
+    name: "이화여대풍물패연합",
+  },
+];
