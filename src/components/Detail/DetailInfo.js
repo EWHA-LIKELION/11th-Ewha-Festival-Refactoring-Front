d1d4770bb53f0e0939c6d4e2cace9834a09eb545
youtheyeon <yuseoyeon@ewhain.net>
2023-10-09 00:53:13 +0900

    chore : 운영시간 주석처리

--- src/components/Detail/DetailInfo.js
+++ src/components/Detail/DetailInfo.js
@@ -27,30 +27,31 @@ const DetailInfo = ({ event, thisData }) => {
       const date = thisData.day[i].date;
       const timeData = thisData.times[i];
 
-      const startTime =
-        timeData.starttime === "null" ? null : new Date(timeData.starttime);
-      const finishTime =
-        timeData.finishtime === "null" ? null : new Date(timeData.finishtime);
+      var formattedStartTime = "";
+      var formattedFinishTime = "";
 
-      if (startTime !== null || finishTime !== null) {
-        const startHour = startTime.getHours();
-        const startMinute = startTime.getMinutes();
-        const finishHour = finishTime.getHours();
-        const finishMinute = finishTime.getMinutes();
-        const formattedStartTime = `${startHour < 12 ? "AM" : "PM"} ${(
-          startHour % 12 || 12
-        )
-          .toString()
-          .padStart(2, "0")}:${startMinute}`;
-        const formattedFinishTime = `${finishHour < 12 ? "AM" : "PM"} ${(
-          finishHour % 12 || 12
-        )
-          .toString()
-          .padStart(2, "0")}:${finishMinute}`;
+      // if (timeData.starttime) {
+      //   const startTime = new Date(timeData.starttime);
+      //   const finishTime = Date(timeData.finishtime);
 
-        const formattedTime = `${date}일 ${day} - ${formattedStartTime} ~ ${formattedFinishTime}`;
-        timesResult[i] = formattedTime;
-      } else timesResult[i] = null;
+      //   const startHour = startTime.getHours();
+      //   const startMinute = startTime.getMinutes();
+      //   const finishHour = finishTime.getHours();
+      //   const finishMinute = finishTime.getMinutes();
+      //   formattedStartTime = `${startHour < 12 ? "AM" : "PM"} ${(
+      //     startHour % 12 || 12
+      //   )
+      //     .toString()
+      //     .padStart(2, "0")}:${startMinute}`;
+      //   formattedFinishTime = `${finishHour < 12 ? "AM" : "PM"} ${(
+      //     finishHour % 12 || 12
+      //   )
+      //     .toString()
+      //     .padStart(2, "0")}:${finishMinute}`;
+      // }
+
+      const formattedTime = `${date}일 ${day} - ${formattedStartTime} ~ ${formattedFinishTime}`;
+      timesResult[i] = formattedTime;
     }
   }
 
