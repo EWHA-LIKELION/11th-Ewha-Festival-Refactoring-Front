26677518297d92c7179775bc9785fec2822bf1cc
y0ung22 <amy6484@ewhain.net>
2023-10-08 23:09:27 +0900

    feat: tf 공지사항 작성 api 연결

--- src/api/tf.js
+++ src/api/tf.js
@@ -12,14 +12,33 @@ export const GetNotice = async () => {
 };
 
 //공지사항 작성
-export const WriteNotice = async () => {
+export const WriteNotice = async (title, content) => {
   try {
+    const response = await http.post(`notices/`, {
+      title: title,
+      content: content,
+    });
+    return response;
   } catch (error) {
     console.error("tf 공지사항 작성 실패", error);
     throw error;
   }
 };
 
-//공지사항 수정
-
 //공지사항 삭제
+export const DeleteNotice = async () => {
+  try {
+  } catch (error) {
+    console.error("tf 공지사항 삭제 실패", error);
+    throw error;
+  }
+};
+
+//공지사항 수정
+export const EditNotice = async () => {
+  try {
+  } catch (error) {
+    console.error("tf 공지사항 수정 실패", error);
+    throw error;
+  }
+};
