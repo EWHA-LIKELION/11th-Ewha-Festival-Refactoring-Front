cc3922619246c39dbd7789a1f248c95b22360fe0
binys21 <binys1121@ewhain.net>
2023-10-09 21:47:48 +0900

    api 수정

---
+++ src/components/_common/ImageSlider.js
@@ -0,0 +1,28 @@
+// import styled from 'styled-components';
+// import {useNavigate} from 'react-router-dom';
+// import Slider from 'react-slick';
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
+
+// const ImageSlider = () => {
+//     const SlideButton = ({ currentSlide, slideCount, children, ...props }) => (
+//         <div {...props}>{children}</div>
+//     );
+
+//     const settings = {
+//         dots: true,
+//         infinite: false,
+//         speed: 500,
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+
+//     };
+//     return (
+//         <>
+//             <StyledSlider {...settings}>
+
+//             </StyledSlider>
+//         </>
+//     );
+// };
+// export default ImageSlider;
